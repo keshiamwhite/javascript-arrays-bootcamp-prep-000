ae0c8219c47d3591113e6529003d028fd77549f2
Keshia White <keshiamwhite@gmail.com>
2018-08-18 21:54:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,8 +20,8 @@ function destructivelyAddElementToEndOfArray(chocolateBars, gummies) {
   return chocolateBars;
 }
 
-function accessElementInArray(chocolateBars){
-  return chocolateBars[1];
+function accessElementInArray(array, index){
+  return (array, index);
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
