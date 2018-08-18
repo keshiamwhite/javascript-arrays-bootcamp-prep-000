5c0837c72577f0912f6652e091785837244f4543
Keshia White <keshiamwhite@gmail.com>
2018-08-18 19:01:10 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,5 @@
+var chocolateBars = ['sncickers', 'hundred grand', 'kitkat', 'skittles'];
+
+var addElementToBeginningOfArray = chocolateBars.unshift('Hershey');
+
+var descructivelyAddElementToBeginningOfArray
